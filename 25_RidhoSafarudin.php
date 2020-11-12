68d3eb311f74fc36174863e4a9405ee4f44ff829
RidhoSafarudin <ridhosafarudin10@gmail.com>
2020-11-12 22:48:21 +0700

    first commit

---
+++ 25_RidhoSafarudin.php
@@ -0,0 +1,18 @@
+<?php
+    $nama = [["Nama"=>"Satria",
+    "kelas"=>"RPL 1"],
+    ["Nama"=>"Arsyi",
+    "kelas"=>"RPL 2"],
+    ["Nama"=>"Dhika",
+    "kelas"=>"RPL 3"],
+    ["Nama"=>"Evan",
+    "kelas"=>"RPL 4"],
+    ["Nama"=>"Fabian",
+    "kelas"=>"RPL 5"],
+    ["Nama"=>"Irfan",
+    "kelas"=>"RPL 6"]];
+
+    echo "<pre>";
+    print_r($nama);
+    echo "</pre>";
+?>
